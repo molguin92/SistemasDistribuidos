20f1314b35d454ec5341f3bec794cba28e75636b
Manuel Olguin <molguin@dcc.uchile.cl>
2015-11-16 14:57:05 -0300

    server ips2

--- Tareas/src/cc5303/tareas/olguin_manuel/GameServer.java
+++ Tareas/src/cc5303/tareas/olguin_manuel/GameServer.java
@@ -21,14 +21,14 @@ public class GameServer {
     // Optional parameter: -n 4 (wait for 4 and start!)
 
     static String urlServer = "rmi://localhost:1099/gameserver";
-    static String serverfile = "./serverIPs";
+    //static String serverfile = "./serverIPs";
+    static String[] serverips = {"192.168.1.183","192.168.1.109"};
 
     public static void main ( String[] args )
     {
         String IP = args[0];
         int n_players = 2;
         boolean together = false;
-        boolean slave = true;
         System.setProperty("java.rmi.server.hostname", IP);
 
         if ( args[1].equalsIgnoreCase("-n") && args.length > 2 )
@@ -37,25 +37,15 @@ public class GameServer {
             together = true;
         }
 
-        BufferedReader br = null;
-        List<String> serverlist = new ArrayList<>();
-        try {
-            br = new BufferedReader(new FileReader(serverfile));
-            String line;
-            while ((line = br.readLine()) != null )
-                if ( !line.equals(IP))
-                    serverlist.add(line);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ArrayList<String> list = new ArrayList<>();
+        for (String server : serverips )
+            if (!server.equals(IP))
+                list.add(server);
 
         GameThread game = new GameThread(n_players, together);
         DistributedGameInterface rinter = null;
         try {
-            rinter = new DistributedGameHandler(game, serverlist.toArray(new String[1]), IP); // TODO Fix
+            rinter = new DistributedGameHandler(game, list.toArray(new String[1]) , IP); // TODO Fix
         } catch (RemoteException e) {
             e.printStackTrace();
         }
