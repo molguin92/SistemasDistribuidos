6fecec4abb8afd873ef67b33c87b1d931d9d7cb2
Manuel Olguin <molguin@dcc.uchile.cl>
2015-11-19 16:00:04 -0300

    debug

--- Tareas/run_server.sh
+++ Tareas/run_server.sh
@@ -2,5 +2,5 @@
 cd src
 javac cc5303/tareas/olguin_manuel/*.java
 rmiregistry &
-java cc5303.tareas.olguin_manuel.GameServer $1 ../iplist.conf -n 4
+java cc5303.tareas.olguin_manuel.GameServer $1 ../iplist.conf -n 2
 killall rmiregistry
