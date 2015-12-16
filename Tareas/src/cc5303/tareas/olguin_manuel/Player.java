4f3c47957b899d21704072ef42844f812ec9cb04
Manuel Olguin <molguin@dcc.uchile.cl>
2015-12-16 10:08:05 -0300

    backing up

--- Tareas/src/cc5303/tareas/olguin_manuel/Player.java
+++ Tareas/src/cc5303/tareas/olguin_manuel/Player.java
@@ -7,7 +7,7 @@ import java.rmi.server.UnicastRemoteObject;
 /**
  * Created by arachnid92 on 03-10-15.
  */
-public class Player extends UnicastRemoteObject implements RemotePlayer
+public class Player extends UnicastRemoteObject implements RemotePlayer, Cloneable
 {
 
     // the logical representation of the player in the game
@@ -111,5 +111,12 @@ public class Player extends UnicastRemoteObject implements RemotePlayer
         this.lives = 4;
 
         this.body = new Rectangle( posX, posY, Player.HW, Player.HW  );
+    }
+
+    @Override
+    public Player clone() throws CloneNotSupportedException {
+        Player p = (Player) super.clone();
+        p.body = (Rectangle) this.body.clone();
+        return p;
     }
 }
