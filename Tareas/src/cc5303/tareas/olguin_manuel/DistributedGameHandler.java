5a56f700b1eaf16dbaf68e5a1bcb3889528a660c
Manuel Olguin <molguin@dcc.uchile.cl>
2015-12-16 12:48:03 -0300

    disconnect

--- Tareas/src/cc5303/tareas/olguin_manuel/DistributedGameHandler.java
+++ Tareas/src/cc5303/tareas/olguin_manuel/DistributedGameHandler.java
@@ -406,7 +406,7 @@ public class DistributedGameHandler extends UnicastRemoteObject implements Distr
                 }
 
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
